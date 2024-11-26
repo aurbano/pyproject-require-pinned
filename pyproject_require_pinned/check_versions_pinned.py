62def92565e03a7007f90ff5402c9f60cba8dd17
Alejandro U. Alvarez <aluralma@gmail.com>
2024-11-26 18:55:32 +0000

    add support for dicts

--- pyproject_require_pinned/check_versions_pinned.py
+++ pyproject_require_pinned/check_versions_pinned.py
@@ -2,20 +2,48 @@ import re
 
 
 def check_versions_pinned(dependencies):
-    version_pattern = re.compile(r"^\d+\..*$")  # Matches versions like '1.0.0'
     unpinned_dependencies = []
     non_develop_local_dependencies = []
 
     for package, version in dependencies.items():
-        if package == "python":
-            continue
-
-        if isinstance(version, dict):
-            # Local packages must be set to develop = True
-            if "path" in version and not version.get("develop", False):
-                non_develop_local_dependencies.append((package, version))
-
-        elif not version_pattern.match(version):
-            unpinned_dependencies.append((package, version))
+        check_version_pinned(
+            unpinned_dependencies, non_develop_local_dependencies, package, version
+        )
 
     return unpinned_dependencies, non_develop_local_dependencies
+
+
+def check_version_pinned(
+    unpinned_dependencies,
+    non_develop_local_dependencies,
+    package,
+    version,
+):
+    if package == "python":
+        return
+
+    version_pattern = re.compile(r"^\d+\..*$")  # Matches versions like '1.0.0'
+
+    if isinstance(version, list):
+        for each_version in version:
+            check_version_pinned(
+                unpinned_dependencies,
+                non_develop_local_dependencies,
+                package,
+                each_version,
+            )
+
+    elif isinstance(version, dict):
+        # Local packages must be set to develop = True
+        if "path" in version and not version.get("develop", False):
+            non_develop_local_dependencies.append((package, version))
+        elif "version" in version:
+            check_version_pinned(
+                unpinned_dependencies,
+                non_develop_local_dependencies,
+                package,
+                version["version"],
+            )
+
+    elif not version_pattern.match(version):
+        unpinned_dependencies.append((package, version))
